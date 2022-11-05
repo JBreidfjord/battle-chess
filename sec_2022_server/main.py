c134abb01d9956ae02dcc8b8ccbc754d08970e37
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-11-05 09:02:27 -0600

    support room tokens

--- sec_2022_server/main.py
+++ sec_2022_server/main.py
@@ -1,13 +1,5 @@
 import uvicorn
-from fastapi import (
-    Cookie,
-    Depends,
-    FastAPI,
-    Query,
-    WebSocket,
-    WebSocketDisconnect,
-    status,
-)
+from fastapi import Depends, FastAPI, Query, WebSocket, WebSocketDisconnect, status
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
@@ -22,15 +14,19 @@ html = """
         <h1>WebSocket Chat</h1>
         <h2>Your ID: <span id="ws-id"></span></h2>
         <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
+            <label>Token: <input type="text" id="token" autocomplete="off" value="some-key-token"/></label>
+            <button onclick="connect(event)">Connect</button>
+            <hr>
+            <label>Message: <input type="text" id="messageText" autocomplete="off"/></label>
             <button>Send</button>
         </form>
         <ul id='messages'>
         </ul>
         <script>
             var client_id = Date.now()
+            var token = document.getElementById("token")
             document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
+            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}?token=${token.value}`);
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
@@ -52,20 +48,31 @@ html = """
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: list[WebSocket] = []
+        self.active_connections: dict[str, list[WebSocket]] = {}
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, websocket: WebSocket, token: str):
+        """Connect to a room with a specific token"""
         await websocket.accept()
-        self.active_connections.append(websocket)
+        if self.active_connections.get(token):
+            # Token already exists, add new connection
+            self.active_connections[token].append(websocket)
+            return
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+        # Otherwise, create new token and add connection
+        self.active_connections[token] = [websocket]
+
+    def disconnect(self, websocket: WebSocket, token: str):
+        """Disconnect a specific connection"""
+        self.active_connections.get(token).remove(websocket)
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
+        """Send a message to a specific connection"""
         await websocket.send_text(message)
 
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
+    async def broadcast(self, message: str, token: str):
+        """Send a message to all connections with a specific token"""
+        print(self.active_connections)
+        for connection in self.active_connections.get(token):
             await connection.send_text(message)
 
 
@@ -77,38 +84,23 @@ async def get():
     return HTMLResponse(html)
 
 
-# async def get_cookie_or_token(
-#     websocket: WebSocket,
-#     session: str | None = Cookie(default=None),
-#     token: str | None = Query(default=None),
-# ):
-#     if session is None and token is None:
-#         await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
-#     return session or token
-
-
-# @app.websocket("/ws")
-# async def websocket_endpoint(
-#     websocket: WebSocket, cookie_or_token: str = Depends(get_cookie_or_token)
-# ):
-#     await websocket.accept()
-#     while True:
-#         data = await websocket.receive_text()
-#         await websocket.send_text(f"Session cookie or query token value is: {cookie_or_token}")
-#         await websocket.send_text(f"Message text was: {data}")
+async def get_token(websocket: WebSocket, token: str | None = Query(default=None)):
+    if token:
+        return token
+    await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
 
 
 @app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    await manager.connect(websocket)
+async def websocket_endpoint(websocket: WebSocket, client_id: int, token: str = Depends(get_token)):
+    await manager.connect(websocket, token)
     try:
         while True:
             data = await websocket.receive_text()
             await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            await manager.broadcast(f"Client #{client_id} says: {data}", token)
     except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
+        manager.disconnect(websocket, token)
+        await manager.broadcast(f"Client #{client_id} left the chat", token)
 
 
 if __name__ == "__main__":

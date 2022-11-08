29f904ba0045c2f28d11d8e0c1faaeff870c59d0
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-11-08 15:25:14 -0700

    add periodic state broadcast

--- sec_2022_server/connection_manager.py
+++ sec_2022_server/connection_manager.py
@@ -1,3 +1,4 @@
+import asyncio
 from typing import Union
 
 from fastapi import WebSocket
@@ -12,6 +13,10 @@ class ConnectionManager:
 
         self.game_managers: dict[str, GameManager] = {}  # dict[token, GameManager]
 
+        self._loop = asyncio.get_event_loop()
+        self._update_time = 1  # seconds
+        self._update_handle: asyncio.TimerHandle | None = None
+
     async def connect(self, websocket: WebSocket, token: str, client_id: str):
         """Connect to a room with a specific token"""
         await websocket.accept()
@@ -22,6 +27,11 @@ class ConnectionManager:
 
         # Otherwise, create new token and add connection
         self.active_connections[token] = {client_id: websocket}
+
+        if not self._update_handle:
+            self._update_handle = self._loop.call_later(
+                self._update_time, self._loop.create_task, self._update_game_states()
+            )
 
     def disconnect(self, token: str, client_id: str):
         """Disconnect a specific connection"""
@@ -73,6 +83,21 @@ class ConnectionManager:
 
         game_state = self.game_managers[token].get_game_state()
         await self.broadcast_json(game_state, token)
+
+    async def _update_game_states(self):
+        # Remove timer if no game managers exist
+        if not self.game_managers:
+            if self._update_handle:
+                self._update_handle.cancel()
+                self._update_handle = None
+            return
+
+        for token in self.game_managers:
+            await self.broadcast_game_state(token)
+
+        self._update_handle = self._loop.call_later(
+            self._update_time, self._loop.create_task, self._update_game_states()
+        )
 
     def update_game(self, token: str, client_id: str, move: dict[str, str]):
         """Update the game state for a specific client"""

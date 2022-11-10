9b2167fd8cfb496e1c5778ec391abb748958b06b
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-11-10 12:43:58 -0700

    add font

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { VitePluginFonts } from "vite-plugin-fonts";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    VitePluginFonts({
+      custom: {
+        families: [
+          {
+            name: "Press Start 2P",
+            local: "Press Start 2P",
+            src: "./src/assets/fonts/PressStart2P-Regular.ttf",
+          },
+        ],
+      },
+    }),
+  ],
+});

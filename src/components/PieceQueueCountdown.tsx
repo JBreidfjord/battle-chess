bf48c5694170fe53bf260013645259bb84fea867
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-11-11 22:40:59 -0700

    add more detail to countdown

--- src/components/PieceQueueCountdown.tsx
+++ src/components/PieceQueueCountdown.tsx
@@ -3,5 +3,9 @@ interface PieceQueueCountdownProps {
 }
 
 export default function PieceQueueCountdown({ count }: PieceQueueCountdownProps) {
-  return <div className="piece-queue-countdown">{count}</div>;
+  return (
+    <div className="piece-queue-countdown">
+      {count === 1 ? <span className="red">Next turn</span> : `${count} turns`}
+    </div>
+  );
 }
